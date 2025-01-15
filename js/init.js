304ab6cad4d59283cd1c97cfd7c4fcf81e2cec50
DIKIY1111325 <libre.dude@gmail.com>
2025-01-15 15:07:51 +0000

    Update init.js

--- js/init.js
+++ js/init.js
@@ -10,11 +10,15 @@ function() {
     // callback function
 });
 */
+console.log("Animation script loaded successfully");
+
+// Ваш код анимации
 $(document).ready(function() {
     console.log("Document is ready");
     
     if ($("#section_2").length) {
         console.log("#section_2 is present");
+        $("#section_2 .text-content").addClass("animated");
     } else {
         console.log("#section_2 is not found");
     }
