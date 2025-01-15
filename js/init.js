475cdf0eceec66261f32d1a9ad1e0dc301615d73
DIKIY1111325 <libre.dude@gmail.com>
2025-01-15 14:50:15 +0000

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,15 +1,15 @@
 //Hook up the tweet display
 
-    /*
-    $(".countdown").countdown({
-        date: "12 March 2024 18:30:00",
-        format: "on"
-    },
-    
-    function() {
-        // callback function
-    });
-    */
+/*
+$(".countdown").countdown({
+    date: "12 March 2024 18:30:00",
+    format: "on"
+},
+
+function() {
+    // callback function
+});
+*/
 $(document).ready(function() {
     console.log("Document is ready");
     
@@ -18,5 +18,4 @@ $(document).ready(function() {
     } else {
         console.log("#section_2 is not found");
     }
-});
 });
